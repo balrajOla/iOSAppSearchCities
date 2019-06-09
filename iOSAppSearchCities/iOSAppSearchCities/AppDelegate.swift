92a76dd6d8d8342937e7a08ce029a602b2b178a2
Balraj Singh <erbalrajs@gmail.com>
2019-06-09 20:17:21 +0530

    Added Home Screen xib

--- iOSAppSearchCities/iOSAppSearchCities/AppDelegate.swift
+++ iOSAppSearchCities/iOSAppSearchCities/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController  = HomeScreenViewController(nibName:"HomeScreenViewController", bundle: Bundle.main)
+        
+        window?.rootViewController = UINavigationController(rootViewController: initialViewController)
+        window?.makeKeyAndVisible()
         return true
     }
 

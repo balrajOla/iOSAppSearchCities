fe7280b525f8f766be89bc09b7872df3beca025b
Balraj Singh <erbalrajs@gmail.com>
2019-06-09 20:11:45 +0530

    Added iOS project

---
+++ iOSAppSearchCities/iOSAppSearchCities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAppSearchCities
+//
+//  Created by Balraj Singh on 09/06/19.
+//  Copyright © 2019 Balraj Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

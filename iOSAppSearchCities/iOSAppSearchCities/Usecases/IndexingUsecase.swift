2294847c0885caae4402a6289f3fc621c43f20a2
Balraj Singh <erbalrajs@gmail.com>
2019-06-11 00:59:47 +0530

    Added check to append only unquie values

--- iOSAppSearchCities/iOSAppSearchCities/Usecases/IndexingUsecase.swift
+++ iOSAppSearchCities/iOSAppSearchCities/Usecases/IndexingUsecase.swift
@@ -32,37 +32,41 @@ struct IndexingUsecase {
         
         serialQueue.async {
             self.service.getAllCities()
-            .bind { (cities: [CityData]) ->  Future<[Bool]> in
-                return whenAll(cities.map { citiesData -> Future<Bool> in
-                    let indexPath = citiesData.getKeyPath()
-                    
-                    return self.indexes.get(forKeyPath: indexPath)
-                        .bind { names -> Future<Bool> in
-                            guard let fileIndex = names.first else {
-                                let fileName = citiesData.getFileName()
-                                return self.service.save(forKey: fileName)([citiesData])
-                                    .bind { _ in
-                                         return self.indexes.save(forKeyPath: indexPath, value: fileName) }
-                            }
-                            
-                            return self.service.get(forKey: fileIndex)
-                                .bind {
-                                    var citiesValue = $0
-                                    citiesValue.append(citiesData)
-                                    return self.service.save(forKey: fileIndex)(citiesValue)
-                            }
+                .bind { (cities: [CityData]) ->  Future<[Bool]> in
+                    return whenAll(cities.map { citiesData -> Future<Bool> in
+                        let indexPath = citiesData.getKeyPath()
+                        
+                        return self.indexes.get(forKeyPath: indexPath)
+                            .bind { names -> Future<Bool> in
+                                guard let fileIndex = names.first else {
+                                    let fileName = citiesData.getFileName()
+                                    return self.service.save(forKey: fileName)([citiesData])
+                                        .bind { _ in
+                                            return self.indexes.save(forKeyPath: indexPath, value: fileName) }
+                                }
+                                
+                                return self.service.get(forKey: fileIndex)
+                                    .bind {
+                                        var citiesValue = $0
+                                        if citiesValue.first(where: { $0.getKeyValue() == citiesData.getKeyValue() }) == nil {
+                                            citiesValue.append(citiesData)
+                                            return self.service.save(forKey: fileIndex)(citiesValue)
+                                        } else {
+                                            return Future<Bool>(value: true)
+                                        }
+                                }
+                        }
+                    })
+                }.fmap { value -> Bool in
+                    AppEnvironment.current.cache[FACache.isIndexingCompleted] = true
+                    return value.reduce(true, { $0 && $1 })
+                }.observe {
+                    switch $0 {
+                    case .success(let success):
+                        response.resolve(with: success)
+                    case .failure(let error):
+                        response.reject(with: error)
                     }
-                })
-            }.fmap { value -> Bool in
-                AppEnvironment.current.cache[FACache.isIndexingCompleted] = true
-                return value.reduce(true, { $0 && $1 })
-            }.observe {
-                switch $0 {
-                case .success(let success):
-                    response.resolve(with: success)
-                case .failure(let error):
-                    response.reject(with: error)
-                }
             }
         }
         
